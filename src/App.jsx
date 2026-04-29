d7ae9eb355968cd3b9e5bd7bd13dc7bbf47d4a94
Farhod Gofurov <fg325609@gmail.com>
2026-04-29 20:22:17 +0500

    jjjjjj

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./Components/Header/Header";
+import Header from "./Components/Header/Header";
+
+  
+
+
+function App(){
+  return(
+    <>
+    <Header />
+    </>
+  )
+
+}
+export default App;
