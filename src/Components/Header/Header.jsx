1cc92c018a2276cc04c5012c18f9239fe5f34426
Farhod Gofurov <fg325609@gmail.com>
2026-04-29 20:30:13 +0500

    jkjjjjj

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,19 @@ import "./Components/Header.scss"
 function Header(){
      return(
         <>
-        
+        <header>
+            <div className="container">
+                <div className="navbar">
+                    <img src="img/logo.svg" alt="" />
+                    <ul>
+                        <li><a href="">HOW WE WORK</a></li>
+                        <li><a href="">BLOG</a></li>
+                        <li><a href="">ACCOUNT</a></li>
+                        <a href="VIEW PLANS">VIEW PLANS</a>
+                    </ul>
+                </div>
+            </div>
+        </header>
         </>
      )
 }
