35eeed0a8cacae7e964d774bda8949d6cd5dc191
Farhod Gofurov <fg325609@gmail.com>
2026-04-30 08:43:48 +0500

    kkkkk

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./Components/Header/Header";
 import Hero from "./Hero/Hero";
+import Sectionone from "./Components/Sectionone/Sectionone"; 
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Sectionone />
     </>
   );
 }
+
 export default App;
---
+++ src/Components/Sectionone/Sectionone.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Sectionone.scss"; 
+
+function Sectionone() {
+    return (
+        <>
+        <section>
+            <div className="container">
+                <div className="two">
+                    <div className="title">
+                        <span></span>
+                        <h1>We’re different</h1>
+                    </div>
+                    <div className="cards">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    );
+}
+
+export default Sectionone;
