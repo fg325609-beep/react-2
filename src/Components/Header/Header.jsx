2bdce05b3010969b75abc1fd5d34dac495d7ffac
Farhod Gofurov <fg325609@gmail.com>
2026-04-30 08:11:42 +0500

    lllll

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,20 @@
 import "./Header.scss"; 
-
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid- Moore-svg-icons";
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
       <div className="container">
         <div className="navbar">
           <h1>INSURE</h1>
-          <ul>
+          
+          <button className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
+            <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+          </button>
+
+          <ul className={isOpen ? "nav-menu active" : "nav-menu"}>
             <li><a href="#">HOW WE WORK</a></li>
             <li><a href="#">BLOG</a></li>
             <li><a href="#">ACCOUNT</a></li>
